0d51458311ef21a211a41e9a81b068c50c03fbea
Arnav <arnavchaturvedi5050@gmail.com>
2024-04-25 11:31:48 -0400

    Init commit!

---
+++ src/main/java/net/arnav/bobon/BobonModClient.java
@@ -0,0 +1,10 @@
+package net.arnav.bobon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BobonModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/arnav/bobon/BobonModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.arnav.bobon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BobonModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
